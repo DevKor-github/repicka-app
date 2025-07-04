a5618aaeccd956e5a73919b5809f5d2c5414c229
halionaz <samhsj@korea.ac.kr>
2025-07-05 00:19:50 +0900

    fix: 화면 스크롤 지멋대로 되는거 방지

--- App.tsx
+++ App.tsx
@@ -4,7 +4,12 @@ import WebView from 'react-native-webview';
 export default function App() {
   return (
     <SafeAreaView style={{flex: 1}}>
-      <WebView source={{uri: 'https://devkor-github.github.io/repicka-web/'}} />
+      <WebView
+        source={{uri: 'https://devkor-github.github.io/repicka-web/'}}
+        scrollEnabled={false}
+        overScrollMode="never"
+        bounces={false}
+      />
     </SafeAreaView>
   );
 }
