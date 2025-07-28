c5ed1c71d18c3c007b6ed39aed8b0ab522b9ac5f
halionaz <samhsj@korea.ac.kr>
2025-07-28 19:17:10 +0900

    feat: 상단바랑 하단바 색깔 지정(안드로이드)

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
+import {StatusBar} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import WebView from 'react-native-webview';
+import * as NavigationBar from 'expo-navigation-bar';
 
 export default function App() {
+  NavigationBar.setBackgroundColorAsync('#1C1C1E');
+  NavigationBar.setButtonStyleAsync('light');
+
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={{flex: 1, backgroundColor: '#1C1C1E'}}>
+      <StatusBar barStyle="light-content" />
       <WebView
         source={{uri: 'https://repicka.netlify.app/'}}
         scrollEnabled={false}
